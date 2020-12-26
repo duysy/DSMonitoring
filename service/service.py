ed5c9f44d0d79be3e5790c3e8a9e3de4d126b5a7
duysy <duyduysysy@gmail.com>
2020-12-26 17:07:58 +0700

    123

--- service/service.py
+++ service/service.py
@@ -84,10 +84,11 @@ class ClientThread(threading.Thread):
                 if(smtpemail.sendEmail(toEmail,subject,content)):
                     id = uuid.uuid1()
                     sqline = sqLine.Sqline()
-                    sqline.execute("INSERT INTO history_notification(id,nameProblem,content,time) VALUES ('{}', '{}', '{}','{}')".format(id,subject, content,time.time()))
-
+                    sqline.execute("INSERT INTO history_notification (id,nameProblem,content,time) VALUES ('{}', '{}', '{}','{}')".format(id,subject, content,time.time()))
                     sqline = sqLine.Sqline()
                     sqline.execute("UPDATE host set activeAtatus = 0 , lastTime='{}' where id = '{}'".format(time.time(),self.host[0]))
+        sqline = sqLine.Sqline()
+        sqline.execute("UPDATE host set activeAtatus = 0  where id = '{}'".format(self.host[0]))
 
 
 class Service:
