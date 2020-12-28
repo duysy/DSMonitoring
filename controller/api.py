5cb2e2437ca8470d6b1c4a22c74474b642298edc
duysy <duyduysysy@gmail.com>
2020-12-29 01:41:24 +0700

    123

--- controller/api.py
+++ controller/api.py
@@ -12,7 +12,9 @@ class Api:
         id = request.args.get('id')
         sqline = sqLine.Sqline()
         oids = sqline.raw("SELECT oid.idOid,oid.name,oid.oid,oid.units,host_oid.value,host_oid.isWorking from host_oid INNER JOIN oid ON host_oid.idOid = oid.idOid WHERE host_oid.id='{}'".format(id))
-        return Response(oids[0][4])
+        response = flask.jsonify({'some': oids[0][4]})
+    	response.headers.add('Access-Control-Allow-Origin', '*')
+    	return response
 
     def api_set_email_ping(self):
         return "hello"
